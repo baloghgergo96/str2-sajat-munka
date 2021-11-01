94c05299115c7502c26a3cef13d57c401337650c
Balogh Gergő <baloghgergo0825@gmail.com>
2021-11-01 10:52:17 +0100

    Update

--- js-1-feladatok-modal/main.js
+++ js-1-feladatok-modal/main.js
@@ -1,8 +1,38 @@
-const modal = document.getElementById("mod-con");
-const openbutton = document.getElementById("myBtn");
+const modal = document.querySelector('#mod-con');
+const openbutton = document.querySelector('#openBtn');
 
-openbutton.addEventlistener('click', openmodal);
+openbutton.addEventListener('click', openmodal);
 
 function openmodal () {
-    modal.style.display = 'block';
+    modal.style= 'display : block'
+};
+
+const exitbutton = document.querySelector('#exitBtn')
+
+exitbutton.addEventListener('click', exitmodal);
+
+function exitmodal () {
+    modal.style = 'display : none';
+};
+
+const okaybutton = document.querySelector('.okay-button')
+
+okaybutton.addEventListener('click', okaymodal);
+
+function okaymodal () {
+    modal.style = 'display : none';
+};
+
+const closebutton = document.querySelector('.close-button')
+
+closebutton.addEventListener('click', closemodal);
+
+function closemodal () {
+    modal.style = 'display : none';
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
 };
